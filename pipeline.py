0e5cd4f5b444be84828bf7befa6647a8ce7e1dfc
Walker McFarlan <walkermcfarlan@walkers-air.lan>
2021-02-23 17:11:39 -0800

    updated pipeline for df merging

--- pipeline.py
+++ pipeline.py
@@ -36,21 +36,66 @@ def find_ratios(df, dum_col):
 
     return (success_keys, success_vals, fail_keys, fail_vals)
 
+# def year_counts(year_lst, df):
+#     df_lst = []
+#     for num, year in enumerate(year_lst):
+#         attack_count = df[(df.iyear >= year) & (df.iyear <= (year + 4))].country_txt.value_counts()
+#         year_df = pd.DataFrame(data = [attack_count.values], columns = attack_count.index)
+#         df + str(num) = year_df.T.rename(columns={0: f'{year} events'})
+#         df_lst.append(df + str(num))
+    
+#     merged_df = pd.concat(df_lst)
+#     return merged_df
+
+def create_combined_frame(primary_df, secondary_df):
+    year_lst = [num for num in range(1970, 2011, 5)]
+    df_lst = []
+    for num, year in enumerate(year_lst):
+        attack_count = primary_df[(primary_df.iyear >= year) & (primary_df.iyear <= (year + 4))].country_txt.value_counts()
+        year_df = pd.DataFrame(data = [attack_count.values], columns = attack_count.index)
+        num = year_df.T.rename(columns={0: f'{year}_events'})
+        df_lst.append(num)
+    
+    merged_df = pd.concat(df_lst, axis=1)
+    merged_df = merged_df.apply(lambda row: row.fillna(row.mean()), axis=1)
+    merged_df['country'] = merged_df.index
+    merged_df.reset_index(drop=True)
+    
+    final_merge = pd.merge(merged_df, secondary_df, left_on='country', right_on='Country Name')
+
+    final_merge['total_attacks'] = round(final_merge['1970_events'] +
+                               final_merge['1975_events'] +
+                               final_merge['1980_events'] +
+                               final_merge['1985_events'] +
+                               final_merge['1990_events'] +
+                               final_merge['1995_events'] +
+                               final_merge['2000_events'] +
+                               final_merge['2005_events'] +
+                               final_merge['2010_events'])
+    return final_merge
+
+def event_count(merged_df):
+
+    merged_df.reset_index(drop=True, inplace=True)
+    
+    event_lst = ['1970_events', '1975_events', '1980_events',
+             '1985_events', '1990_events', '1995_events', '2000_events',
+             '2005_events', '2010_events']
+    
+    edu_lst = ['1970', '1975', '1980', '1985', '1990',
+           '1995', '2000', '2005', '2010']
+    
+    country_lst = merged_df['country'].tolist()
+    
+    data_dct = {'education': [], 'nattacks': []}
+    for idx, _ in enumerate(country_lst):
+        for atk in merged_df.loc[idx, event_lst]:
+            data_dct['nattacks'].append(atk)
+        for edu in merged_df.loc[idx, edu_lst]:
+            data_dct['education'].append(edu)
+    
+    atk_vs_edu = pd.DataFrame.from_dict(data_dct)
+    return atk_vs_edu
 
 if __name__ == "__main__":
-    df = pd.read_csv('data/terror_db.csv')
-
-    post_df = df[
-        (df.eventid >= 200109110004) & 
-        (df.doubtterr == 0) &
-        (df.attacktype1 != 9) &
-        (df.weaptype1 != 13)
-        ]
-
-    top_df = post_df[
-        (post_df.country_txt == 'Iraq') | 
-        (post_df.country_txt == 'Pakistan') | 
-        (post_df.country_txt == 'Afghanistan') | 
-        (post_df.country_txt == 'India') | 
-        (post_df.country_txt == 'Colombia')
-        ]
+    print('main')
